0279c279614d3015cd5afc955607b1d53e4c4dc2
Mathew Goldsborough <mat@onlyonet.com>
2024-02-12 22:52:10 -1000

    pricing, careers, and footer updates

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+export const config = {
+  bookingLink: "https://calendly.com/threadscribeai/mat-intro",
+  social: {
+    linkedIn: "https://www.linkedin.com/company/threadscribeai",
+    facebook: "https://www.facebook.com/threadscribeai",
+    twitter: "https://www.twitter.com/threadscribeai",
+  },
+  threadScribe: {
+    slack: "https://threadscribeai.slack.com",
+    appHome: "https://app.threadscribe.ai",
+    installLink: "https://bot.threadscribe.ai/slack/install",
+  },
+};
