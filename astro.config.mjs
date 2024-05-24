e4c732fd7dc8835afaa457e2d850db1649211caa
Mathew Goldsborough <mat@onlyonet.com>
2024-05-23 21:55:52 -1000

    meta updates for all pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.threadscribe.ai'
+  site: "https://www.threadscribe.ai",
+  srcDir: "./src",
 });
