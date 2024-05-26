ce5efcb43dae4c789fffe9245520bba72377122d
Mathew Goldsborough <mat@onlyonet.com>
2024-05-25 15:23:40 -1000

    adding sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.threadscribe.ai",
   srcDir: "./src",
+  integrations: [
+    sitemap({
+      filter: (page) =>
+        page !== "https://www.threadscribe.ai/thank-you-linkedin/" &&
+        page !== "https://www.threadscribe.ai/thank-you/" &&
+        page !== "https://www.threadscribe.ai/builder-preview/",
+    }),
+  ],
 });
